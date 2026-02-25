bc0018f37f550ff1994baf8b8773971b03b940f4
Markuss Watters <markusswatters06@gmail.com>
2026-02-25 16:30:28 +0000

    Implement tests for Workout class in main.cpp
    Added tests for Workout class constructor and setters.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    
+    // Testing Constructor
+    Workout w1("Bench Press", 3, 12, 120, 57, 5, 6);
+    //w1.displayDetails();
+    w1.displayDetailsTest();
 
+    // Testing Setters
+    Workout w2;
+    w2.setExercise("Squats");
+    w2.setSets(6);
+    w2.setReps(8);
+    w2.setWeight(100);
+    w2.setMins(25);
+    w2.setTenSeconds(4);
+    w2.setSeconds(9);
+    w2.displayDetailsTest();
+    cin >> x;
+
+    return 0;
 };
