6471e7444c9165cccef796f628666035ec44aa4f
Markuss Watters <markusswatters06@gmail.com>
2026-03-30 22:18:16 +0100

    Refactor Login class for user ID handling
    Updated Login class constructors and function parameters to include user ID.

--- Login.h
+++ Login.h
@@ -2,18 +2,18 @@
 #define LOGIN_H
 
 #include "User.h"
-
 #include <iostream>
 using namespace std;
 
-class Login : public User{
-    public:
-        // Constructors
-        Login();
-        Login(int i, string u, string p);
+class Login : public User {
+public:
+    // Constructors
+    Login();
+    Login(int i, string p);
 
-        // Functions
-        bool loginUser(string u, string p);
-        void loggingIn(string &u, string &p);
+    
+    // Functions
+    bool loginUser(int i, string p);
+    void loggingIn(int &i, string &p);
 };
 #endif
