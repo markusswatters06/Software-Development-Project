ebd6133c98830eaac196d1e5698851395606ed26
Markuss Watters <markusswatters06@gmail.com>
2026-03-30 22:18:51 +0100

    Enhance main.cpp with detailed testing outputs
    Updated main function to include more detailed user testing and account registration.

--- main.cpp
+++ main.cpp
@@ -7,25 +7,39 @@ int x;
 int main()
 {
     // Testing User
-    User u1(1, "user1", "pass1");
+    cout << "---Testing User Constructor---" << endl;
+    User u1(1, "name1", "surname1", "user1@email.com", 891, 30 , 03, 2026, "pass1");
     u1.displayDetails();
 
-    
+
     // Testing Signup
     Signup s;
+    cout << "\n---Registering an Account---" << endl;
     s.registerAccount();
     s.displayDetails();
 
 
     // Testing Login
     Login l;
+    cout << "\n---Testing Login---" << endl;
     l.setId(s.getId());
-    l.setUsername(s.getUsername());
+    l.setFirstName(s.getFirstName());
+    l.setSurname(s.getSurname());
+    l.setPhone(s.getPhone());
+    l.setEmail(s.getEmail());
     l.setPassword(s.getPassword());
-    string u, p;
-    l.loggingIn(u,p);
-    l.loginUser(u,p);
+    l.setDobDay(s.getDobDay());
+    l.setDobMonth(s.getDobMonth());
+    l.setDobYear(s.getDobYear());
 
+    int i;
+    string p;
+    cout << "\n---Testing Logging In---" << endl;
+    l.loggingIn(i,p);
+    l.loginUser(i,p);
+    cout << "\n---Test Successful---" << endl;
+    
+    l.displayDetails();
 
 
     cin.ignore();
