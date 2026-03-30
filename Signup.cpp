f0fc9399762820a8dce460392cf58d20f67430a5
Markuss Watters <markusswatters06@gmail.com>
2026-03-30 22:17:51 +0100

    Enhance Signup class to include additional user details

--- Signup.cpp
+++ Signup.cpp
@@ -2,23 +2,45 @@
 
 Signup::Signup() : User(){}
 
-Signup::Signup(int i, string u, string p):
-    User(i,u,p){}
+Signup::Signup(int i,string fn, string sn, string e, int ph, int dd, int dm, int dy, string p)
+    : User(i,fn,sn,e,ph,dd,dm,dy,p){}
 
 void Signup::registerAccount()
 {
-    int i;
-    string u,p;
-    cout << "\nEnter ID: " << endl;
+    int i, ph, dd, dm, dy;
+    string fn, sn, e, p;
+
+    cout << "\nEnter Name " << endl;
+    cout << "(First Name): ";
+    cin >> fn;
+
+    cout << "(Surname): ";   
+    cin >> sn;
+
+    cout << "\nEnter Email Address: ";
+    cin >> e;
+
+    cout << "\nEnter Phone Number: ";
+    cin >> ph;
+    
+    cout << "Enter Date of Birth\n";
+    getValidDate(dd, dm, dy);
+
+    cout << "\nEnter ID: ";
     cin >> i;
-    cout << "\nEnter Username: " << endl;
-    cin >> u;
-    cout << "\nEnter password " << endl;
+
+    cout << "\nEnter Password: "; 
     cin >> p;
     
     setId(i);
-    setUsername(u);
+    setFirstName(fn);
+    setSurname(sn);
+    setEmail(e);
+    setPhone(ph);
+    setDobDay(dd);
+    setDobMonth(dm);
+    setDobYear(dy);
     setPassword(p);
 
-    cout << "\n\nSignup Successful!" << endl;
+    cout << "\nSignup Successful!" << endl;
 }
