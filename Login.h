216821332501e89affcb575252a13e516ae613e4
Markuss Watters <markusswatters06@gmail.com>
2026-03-30 14:59:52 +0100

    Refactor Login class to inherit from User

--- Login.h
+++ Login.h
@@ -1,30 +1,19 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
+#include "User.h"
+
 #include <iostream>
 using namespace std;
 
-class Login {
-    private:
-        int id;
-        string username;
-        string password;
-
+class Login : public User{
     public:
-        // Constructors/Destructors
-        Login();    // Default Constructor
-        Login(int i, string u, string p);  // Overloaded Constructor
-        ~Login();   // Destructor
+        // Constructors
+        Login();
+        Login(int i, string u, string p);
 
-        // Getters
-        string getUsername(){return username;}  // Username
-        string getPassword(){return password;}  // Password
-        int getId(){return id;} // ID
-
-        // Setters
-        void setUsername(string u){username = u;}   // Username
-        void setPassword(string p){password = p;}   // Password
-        void setId(int i){id = i;}  // ID
-
+        // Functions
+        bool loginUser(string u, string p);
+        void displayLogin(string &u, string &p);
 };
-#endif 
+#endif
