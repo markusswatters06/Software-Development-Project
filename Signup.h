5b954919d38a426ed3ac9d698d95162c04428703
Markuss Watters <markusswatters06@gmail.com>
2026-03-30 22:17:35 +0100

    Enhance Signup class with additional constructor parameters

--- Signup.h
+++ Signup.h
@@ -8,9 +8,11 @@ using namespace std;
 
 class Signup : public User{
     public:
+        // Constructors
         Signup();
-        Signup(int i, string u, string p);
+        Signup(int i,string fn, string sn, string e, int ph, int dd, int dm, int dy, string p);
 
+        // Functions
         void registerAccount();
 };
 #endif
