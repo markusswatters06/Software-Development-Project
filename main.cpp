3641f2e2fe828da167114b6001014aeabd7f8fff
Markuss Watters <markusswatters06@gmail.com>
2026-04-02 14:39:40 +0100

    Refactor main.cpp to include Client testing

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include "User.h"
+#include "Client.h"
 
 int x;
 
 int main()
 {
-    // Testing User
+/*// Testing User
     cout << "---Testing User Constructor---" << endl;
     User u1(1, "name1", "user1@email.com", 891, 30 , 03, 2026, "pass1");
-    u1.displayDetails();
+    u1.displayMenu();
 
-    // Testing Signup
+// Testing Signup
     User s;
     s.registerAccount();
     s.displayDetails();
 
 
-    // Testing Login
+// Testing Login
     User l;
     cout << "\n---Testing Login---" << endl;
     l.setId(s.getId());
@@ -32,12 +33,22 @@ int main()
     cout << "\n---Testing Logging In---" << endl;
     l.loggingIn(i,p);
     l.loginUser(i,p);
-    cout << "\n---Test Successful---" << endl;
-    
+    cout << "\n---Test Successful---" << endl; 
     l.displayDetails();
 
 
-    cin.ignore();
+// Testing Client
+    User u4(1, "name1", "user1@email.com", 891, 30 , 03, 2026, "pass1");*/
+    Client c1(1, "name1", "email1@email.com", 1234567890, 1, 1, 2000, "pass", 192.1, 90, "Gym", "Goal 1", 1, 4, 2026);
+    c1.displayMenu();
+    //c1.displayDetails();
+    //c1.membershipMenu();
+    //c1.progressMenu();
+    //c1.workoutMenu();
+    //c1.sessionMenu();
+
+
+
     cin.get();
     return 0; 
 };
