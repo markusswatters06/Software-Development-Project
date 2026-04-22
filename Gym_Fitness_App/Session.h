670f652f6d4d698acb3ec892f1e13e5000338470
Markuss Watters <markusswatters06@gmail.com>
2026-04-22 14:15:23 +0100

    Enhance Session class with static members and operators
    Added static members for session management and operator overloading.

--- Gym_Fitness_App/Session.h
+++ Gym_Fitness_App/Session.h
@@ -2,56 +2,77 @@
 #define SESSION_H
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
 using namespace std;
 
-class Session{
-    private:
-        int id;
-        string sessionName;
-        string trainerName;
-        int sessionDay;
-        int sessionMonth;
-        int sessionYear;
-        string time;
-        int spacesAvailable;
-    
-    public:
-        // Constructors & Destructors
-        Session();
-        Session(int i, string sn, string tn, int sd, int sm, int sy, string t, int sa);
-        ~Session();
+class Session {
+private:
+    static int nextSessionId;
+    int id;
+    string sessionName;
+    string trainerName;
+    int sessionDay;
+    int sessionMonth;
+    int sessionYear;
+    string time;
+    int spacesAvailable;
 
+    static vector<Session> sessionList;
+    static int sessionIndex(int sessionId);
 
-        // Getters
-        int getId(){return id;}
-        string getSessionName(){return sessionName;}
-        string getTrainerName(){return trainerName;}
-        int getSessionDay(){return sessionDay;}
-        int getSessionMonth(){return sessionMonth;}
-        int getSessionYear(){return sessionYear;}
-        string getTime(){return time;}
-        int getSpacesAvailable(){return spacesAvailable;}
+public:
+    // Constructors & Destructor
+    Session();
+    Session(int i, string sn, string tn, int sd, int sm, int sy, string t, int sa);
+    ~Session();
 
-        // Setters
-        void setId(int sid){id = sid;}
-        void setSessionName(string sn){sessionName = sn;}
-        void setTrainerName(string tn){trainerName = tn;}
-        void setSessionDay(int sd){sessionDay = sd;}
-        void setSessionMonth(int sm){sessionMonth = sm;}
-        void setSessionYear(int sy){sessionYear = sy;}
-        void setTime(string st){time = st;}
-        void setSpacesAvailable(int sa){spacesAvailable = sa;}
+    // Operator Overloading
+    Session& operator--();
+    Session operator--(int);
+    Session& operator++();
+    Session operator++(int);
+    bool operator==(const Session& other) const;
+    friend ostream& operator<<(ostream& os, const Session& session);
 
+    // Getters
+    int getId() const;
+    string getSessionName() const;
+    string getTrainerName() const;
+    int getSessionDay() const;
+    int getSessionMonth() const;
+    int getSessionYear() const;
+    string getTime() const;
+    int getSpacesAvailable() const;
 
-        // Functions
-        void createSession();
-        void displaySession();
-        void editSession();
-        void bookSession();
-        bool getValidDate(int& dd, int& dm, int& dy);
-        
-        //UI
-        void clearScreen();
-        void line();
+    // Setters
+    void setId(int sid);
+    void setSessionName(string sn);
+    void setTrainerName(string tn);
+    void setSessionDay(int sd);
+    void setSessionMonth(int sm);
+    void setSessionYear(int sy);
+    void setTime(string st);
+    void setSpacesAvailable(int sa);
+
+    // Functions
+    void createSession();
+    void addSession(const Session& session);
+    void removeSession();
+    void displaySession() const;
+    static bool hasSessions();
+    bool bookSessionById(int sessionId);
+    bool unbookSessionById(int sessionId);
+    void editSession();
+    void bookSession();
+    void unbookSession();
+    bool getValidDate(int& d, int& m, int& y);
+    void validateSession() const;
+
+    // UI
+    void clearScreen();
+    void line() const;
 };
+
 #endif
