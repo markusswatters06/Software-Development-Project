a8ae7a89f35049e082019bfedc8baa9c619fc31f
Markuss Watters <markusswatters06@gmail.com>
2026-04-26 19:58:13 +0100

    Refactor Client class with new workout and goal features
    Final

--- Gym_Fitness_App/Client.h
+++ Gym_Fitness_App/Client.h
@@ -6,21 +6,33 @@
 #include "User.h"
 #include "Session.h"
 #include "Goal.h"
+#include "Cardio.h"
+#include "Exercise.h"
+#include "WorkoutPlan.h"
+#include "WorkoutExercise.h"
 
 class Client : public User{
     private:
         double height, weight;
-        string membershipType, goal;
-        int expiryDay, expiryMonth, expiryYear;
+        string goal;
         vector<int> bookedSessionIds;
+        vector<Goal> clientGoals;
         Session* bookedSession;
         Goal* currentGoal;
+        Cardio cardioWorkout;
+        Exercise currentExercise;
+        WorkoutPlan currentWorkoutPlan;
+        WorkoutExercise currentWorkoutExercise;
+
+        void initializeWorkoutData();
+        void syncWorkoutLinks();
+        //void printProfileDetails() const;
 
     public:
         // Constructors & Destructors
         Client(); 
-        Client(double h, double w, string mt, string g, int ed, int em, int ey); 
-        Client(int i, string n, string e, int ph, int dd, int dm, int dy, string p, double h, double w, string mt, string g, int ed, int em, int ey); 
+        Client(double h, double w, string g); 
+        Client(int i, string n, string e, int ph, int dd, int dm, int dy, string p, double h, double w, string g); 
         Client(const Client& other);
         Client& operator=(const Client& other);
         ~Client();
@@ -33,19 +45,17 @@ class Client : public User{
         // Getters
         double getHeight(){return height;}
         double getWeight(){return weight;}
-        string getMembershipType(){return membershipType;}
         string getGoal(){return goal;}
-        void getExpiryDate(int &ed, int &em, int &ey){ed = expiryDay; em = expiryMonth; ey = expiryYear;}
+        const vector<Goal>& getClientGoals() const { return clientGoals; }
 
 
         // Setters
         void setHeight(double h){height = h;}
         void setWeight(double w){weight = w;}
-        void setMembershipType(string mt){membershipType = mt;}
         void setGoal(string g){goal = g;}
-        void setExpiryDate(int ed, int em, int ey){expiryDay = ed; expiryMonth = em; expiryYear = ey;}
         void setBookedSession(const Session& session);
         void setCurrentGoal(const Goal& newGoal);
+        void setWorkoutTester(const WorkoutPlan& workoutPlan, string exerciseName, int sets, int reps, int restTime);
         bool hasBookedSession(int sessionId) const;
 
 
@@ -53,10 +63,9 @@ class Client : public User{
         void registerAccount() override;
         
         // Menus
-        void displayMenu() override;
+        virtual void displayMenu();
         void workoutMenu();
         void sessionMenu();
-        void membershipMenu();
         void goalMenu();
 
         // Profile
@@ -66,14 +75,21 @@ class Client : public User{
 
 
         // Workouts
-        void displayWorkout();
+        void displayWorkouts();
+        void displayCurrentWorkout() const;
+        void createWorkout();
         void editWorkout();
-        void logWorkout();
+        void removeWorkout();
 
         // Goals
-        void addGoal();
+        void addGoal(const Goal& goal);
+        void editGoal();
+        void removeGoal();
         void displayGoal();
+        void displayGoals();
         void updateProgress();
+        void updateDeadline();
+        void updateStatus();
         void checkProgress();
 
         //Sessions
@@ -85,6 +101,7 @@ class Client : public User{
         // UI
         void clearScreen();
         void line();
-        void title(string t);
+        void lineLong();
+        void dashedLine();
 };
 #endif
