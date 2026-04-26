723fc72225bf74799eecac99fbb10fc4f369491a
Markuss Watters <markusswatters06@gmail.com>
2026-04-26 19:53:24 +0100

    Refactor User class and update member functions
    Final

--- Gym_Fitness_App/User.h
+++ Gym_Fitness_App/User.h
@@ -9,19 +9,15 @@ class User {
     protected:
         static int nextUserId;
         int id;
-        string name;
-        string email;
-        int phone;
-        int dobDay;
-        int dobMonth;
-        int dobYear;
+        string name, email;
+        int phone, dobDay, dobMonth, dobYear;
         string password;
 
     public:
         // Constructors & Destructors
         User();                             // Default Constructor
         User(int i, string n, string e, int ph, int dd, int dm, int dy, string p);    // Overloaded Constructor
-        ~User();                            // Destructor
+        virtual ~User();                    // Destructor
 
 
         // Getters
@@ -31,7 +27,7 @@ class User {
         int getPhone(){return phone;}               // Phone
         int getDobDay(){return dobDay;}             // DOB Day
         int getDobMonth(){return dobMonth;}         // DOB Month
-        int getDobYear(){return dobYear;}           // DOB Year
+        int getDobYear(){return dobYear;} 
         string getPassword(){return password;}      // Password
 
 
@@ -42,21 +38,28 @@ class User {
         void setPhone(int ph){phone = ph;}              // Phone
         void setDobDay(int dd){dobDay = dd;}            // DOB Day
         void setDobMonth(int dm){dobMonth = dm;}        // DOB Month
-        void setDobYear(int dy){dobYear = dy;}          // DOB Year
+        void setDobYear(int dy){dobYear = dy;}   
         void setPassword(string p){password = p;}       // Password
 
 
         // Functions
-        virtual void displayMenu();
+        void mainMenu();
         virtual void displayDetails();
         virtual void registerAccount();
         bool verifyUser(int i, string p);
         void logIn(int &i, string &p);
         bool getValidDate(int& dd, int& dm, int& dy);
 
+        // Menus
+        virtual void displayMenu();
+        virtual void goalMenu();
+        virtual void workoutMenu();
+
         // UI
         void clearScreen();
         void line();
-        void title(string t); 
+        void lineLong();
+        void dashedLine(); 
 };
+
 #endif 
